ebe650ce3df0c08d5b9b9b35d333839dbd348b67
Skinner <Nick.Skinner@dnvgl.com>
2017-11-15 12:05:39 -0800

    More descriptive comment for about

--- PHP/About.php
+++ PHP/About.php
@@ -2,9 +2,10 @@
 <? 
     include("header.php");
 ?>
-<!-- Style -->
+
 <style>
 <?php include 'index.css'; ?>
+<!-- header -->
 </style>
 <div class="topnav">
   <a class="active" href="Home.php">Home</a>
@@ -12,7 +13,7 @@
   <a href="Account.php">Account</a>
   <a href="About.php">About</a>
 </div>
-<!-- end Style-->
+<!-- end header -->
 <?php
 echo "<h1>About Us</h1>";
 echo "<h2>Here at nickland, we want to provide a digital cookbook for those who want to share their recipes</h2>";
