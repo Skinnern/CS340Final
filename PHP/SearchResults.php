46d1c3cf97eb30954f45bb15ac9065691df9da09
Skinnern <Skinnern@oregonstate.edu>
2017-11-16 13:17:39 -0800

    add user complete
    can add users, user automatically gets user id

---
+++ PHP/SearchResults.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<!-- Recipe-->
+<html>
+<? 
+    include("header.php");
+?>
+<!-- Style -->
+<style>
+<?php include 'index.css'; ?>
+</style>
+<div class="topnav">
+  <a class="active" href="Home.php">Home</a>
+  <a href="RecipesSearch.php">Recipe Search</a>
+  <a href="Account.php">Account</a>
+  <a href="About.php">About</a>
+</div>
+<!-- end Style-->
+<?php
+	include 'connectvarsEECS.php'; 
+	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	if (!$conn) {
+		die('Could not connect: ' . mysql_error());
+	}
+	
+	//query to execute
+	$query = "select U.USERNAME, r.RECIPE_NAME, r.RECIPE_COST from RECIPE r, USER U where r.USER_ID = U.USER_ID LIMIT 0,10";
+	$result = mysqli_query($conn, $query);
+	if (!$result) {
+		die("Query to show fields from table failed");
+	}
+	
+	//number of fields
+	$fields_num = mysqli_num_fields($result);
+	echo "<h1>Popular Recipes: $table </h1>";
+	echo "<table id='t01' border='1'><tr>";
+	
+	//table headers
+	echo "<td><b>User</b></td>";	
+	echo "<td><b>Recipe Name</b></td>";
+	echo "<td><b>Total Cost</b></td>";
+	echo "</tr>\n";
+	
+	//table content
+	while($row = mysqli_fetch_row($result)) {	
+		echo "<tr>";	
+		// $row is array... foreach( .. ) puts every element
+		// of $row to $cell variable	
+		foreach($row as $cell)		
+			echo "<td>$cell</td>";	
+		echo "</tr>\n";
+	}
+
+	mysqli_free_result($result);
+	mysqli_close($conn);
+?>
+<? 
+    include("footer.php");
+?>
+</html>
+
+	
---
+++ PHP/insertUser.php
@@ -0,0 +1,94 @@
+﻿<!DOCTYPE html>
+<!-- new account -->
+<html>
+<? 
+    include("header.php");
+?>
+
+<style>
+<?php include 'index.css'; ?>
+</style>
+<div class="topnav">
+  <a class="active" href="Home.php">Home</a>
+  <a href="RecipesSearch.php">Recipe Search</a>
+  <a href="Account.php">Account</a>
+  <a href="About.php">About</a>
+</div>
+
+<div>
+<h2>Add User</h2>
+<?php
+// change the value of $dbuser and $dbpass to your username and password
+	include 'connectvarsEECS.php'; 
+	
+	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	if (!$conn) {
+		die('Could not connect: ' . mysql_error());
+	}
+
+	
+	
+	
+// Escape user inputs for security
+	$username = mysqli_real_escape_string($conn, $_POST['username']);
+	$email = mysqli_real_escape_string($conn, $_POST['email']);
+	$password = mysqli_real_escape_string($conn, $_POST['password']);
+	
+	//end age	
+	
+//check for unique username
+	$numberOfErrors = 0;
+	$queryUnique = "select username from Users where username = '$username'";
+	//check not empty
+	if(empty($username) == true){
+		echo "
+		please enter a username\n";
+		$numberOfErrors++;
+	}
+	 if(empty($email) == true){
+		echo "
+		please enter an email\n";
+		$numberOfErrors++;
+	}
+	 if(empty($password) == true){
+		echo "
+		please enter a password\n";
+		$numberOfErrors++;
+	}
+	
+	//if we have errors, do not let the user proceed
+	if($numberOfErrors < 1) {
+		if(mysqli_query($conn, $queryUnique)){
+			//echo "Connected.";
+			//fbsql_affected_rows
+			$result = mysqli_query($conn, $queryUnique);
+			
+			//testing rows retrieved
+			if (mysqli_num_rows($result) > 0) {
+				echo "
+				Error: username already exists in database, please retry with a different user";
+				mysqli_close($conn);		
+			} else{
+				// attempt insert query 
+				$query = "INSERT INTO USER (username, email, password) VALUES ('$username', '$email', '$password')";
+				if(mysqli_query($conn, $query)){
+					echo "New User added successfully!";
+				} else{
+					echo "ERROR: Could not able to execute $query. " . mysqli_error($conn);
+				}
+			}
+			
+		} else{
+			echo "ERROR: Could not able to execute $queryUnique. " . mysqli_error($conn);
+		}
+	}
+// close connection
+mysqli_close($conn);
+?>
+<div>
+<? 
+    include("footer.php");
+?>
+</html>
+
+	
