807c120fe818575f954b2ad8b535ab15f21e41f9
Skinner <Nick.Skinner@dnvgl.com>
2017-11-20 08:14:29 -0800

    Project SQL for HW due 11/20/2017

---
+++ PHP/PROJECT-SQL.sql
@@ -0,0 +1,27 @@
+--*************************************
+--SELECTS
+--*************************************
+
+--Home Page top 10 recipies
+select U.USERNAME, r.RECIPE_NAME, r.RECIPE_COST from RECIPE r, USER U where r.USER_ID = U.USER_ID LIMIT 0,10;
+
+--Search Result
+--get my most recent update
+
+--Third select statement
+
+
+--*************************************
+--INSERT
+--*************************************
+
+--Create Acount
+--INSERT
+
+
+--*************************************
+--UPDATE
+--*************************************
+
+--upsert ingredient
+
