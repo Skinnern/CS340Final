f373c95d69d8bbbdeb31eceba305ec4dfec8b8e1
Skinnern <Skinnern@oregonstate.edu>
2017-11-24 07:54:10 -0800

    few fixes, also added some prints for checking submit recipe, seems to be working well so far

--- PHP/BuildRecipe.php
+++ PHP/BuildRecipe.php
@@ -39,7 +39,12 @@ if ($Stepnum == 0) {
 echo "please retry with a valid number of steps.";
 } else{
 ?>
-<form action="SubmitRecipe.php" method="post">
+<form action="SubmitRecipe.php?numstepsagain=<?php echo $Stepnum; ?>" method="post">
+		<label for="Rname">Recipe Name:</label>
+		<p>
+			</p>
+			<input type="text" id="Rname" name="Rname" placeholder="Enter Recipe Name...">
+
 	<!-- Step 1 -->
 	<?php if($Stepnum >= 1) {?>
 		<label for="Step1">Step 1:</label>
@@ -48,9 +53,9 @@ echo "please retry with a valid number of steps.";
 			<input type="text" id="Step1" name="Step1" placeholder="Enter Step Instructions...">
 		<!-- Dropdown -->
 		<br />
-		<span class="tab"></span><label for="Locationof">Ingredient (optional)</label>
+		<span class="tab"></span><label for="Iname1">Ingredient (optional)</label>
 		<br />
-			<span class="tab"></span><select name="Step1" id="Step1">
+			<span class="tab"></span><select name="Iname1" id="Iname1">
 				<option value = "0" selected = "selected">Select...</option>
 				<?php
 				while($row = mysqli_fetch_row($result)) {	
@@ -70,17 +75,24 @@ echo "please retry with a valid number of steps.";
 		<span class="tab"></span><input type="text" id="ingredamount1" name="ingredamount1" placeholder="1...">
 	<?php } ?>
 	<!-- End Step 1 -->	
-		<!-- Step 2 -->
-	<?php if($Stepnum >= 2) {?>
+	<!-- Step 2 -->
+	<?php if($Stepnum >= 2) {
+		$query = "select Ingredient_name from INGREDIENT";
+		$result = mysqli_query($conn, $query);
+		if (!$result) {
+			die("Query to show fields from table failed");
+		}
+?>
 		<label for="Step2">Step 2:</label>
+		
 		<p>
 			</p>
 			<input type="text" id="Step2" name="Step2" placeholder="Enter Step Instructions...">
 		<!-- Dropdown -->
 		<br />
-		<span class="tab"></span><label for="Locationof">Ingredient (optional)</label>
+		<span class="tab"></span><label for="Iname2">Ingredient (optional)</label>
 		<br />
-			<span class="tab"></span><select name="Step2" id="Step2">
+			<span class="tab"></span><select name="Iname2" id="Iname2">
 				<option value = "0" selected = "selected">Select...</option>
 				<?php
 				while($row = mysqli_fetch_row($result)) {	
@@ -100,17 +112,23 @@ echo "please retry with a valid number of steps.";
 		<span class="tab"></span><input type="text" id="ingredamount2" name="ingredamount2" placeholder="2...">
 	<?php } ?>
 	<!-- End Step 2 -->	
-		<!-- Step 3 -->
-	<?php if($Stepnum >= 3) {?>
+	<!-- Step 3 -->
+	<?php if($Stepnum >= 3) {
+		$query = "select Ingredient_name from INGREDIENT";
+		$result = mysqli_query($conn, $query);
+		if (!$result) {
+			die("Query to show fields from table failed");
+		}
+?>
 		<label for="Step3">Step 3:</label>
 		<p>
 			</p>
 			<input type="text" id="Step3" name="Step3" placeholder="Enter Step Instructions...">
 		<!-- Dropdown -->
 		<br />
-		<span class="tab"></span><label for="Locationof">Ingredient (optional)</label>
+		<span class="tab"></span><label for="Iname3">Ingredient (optional)</label>
 		<br />
-			<span class="tab"></span><select name="Step3" id="Step3">
+			<span class="tab"></span><select name="Iname3" id="Iname3">
 				<option value = "0" selected = "selected">Select...</option>
 				<?php
 				while($row = mysqli_fetch_row($result)) {	
@@ -130,17 +148,23 @@ echo "please retry with a valid number of steps.";
 		<span class="tab"></span><input type="text" id="ingredamount3" name="ingredamount3" placeholder="3...">
 	<?php } ?>
 	<!-- End Step 3 -->	
-		<!-- Step 4 -->
-	<?php if($Stepnum >= 4) {?>
+	<!-- Step 4 -->
+	<?php if($Stepnum >= 4) {
+		$query = "select Ingredient_name from INGREDIENT";
+		$result = mysqli_query($conn, $query);
+		if (!$result) {
+			die("Query to show fields from table failed");
+		}
+?>
 		<label for="Step4">Step 4:</label>
 		<p>
 			</p>
 			<input type="text" id="Step4" name="Step4" placeholder="Enter Step Instructions...">
 		<!-- Dropdown -->
 		<br />
-		<span class="tab"></span><label for="Locationof">Ingredient (optional)</label>
+		<span class="tab"></span><label for="Iname4">Ingredient (optional)</label>
 		<br />
-			<span class="tab"></span><select name="Step4" id="Step4">
+			<span class="tab"></span><select name="Iname4" id="Iname4">
 				<option value = "0" selected = "selected">Select...</option>
 				<?php
 				while($row = mysqli_fetch_row($result)) {	
@@ -160,17 +184,23 @@ echo "please retry with a valid number of steps.";
 		<span class="tab"></span><input type="text" id="ingredamount4" name="ingredamount4" placeholder="4...">
 	<?php } ?>
 	<!-- End Step 4 -->	
-		<!-- Step 5 -->
-	<?php if($Stepnum >= 5) {?>
+	<!-- Step 5 -->
+	<?php if($Stepnum >= 5) {
+		$query = "select Ingredient_name from INGREDIENT";
+		$result = mysqli_query($conn, $query);
+		if (!$result) {
+			die("Query to show fields from table failed");
+		}
+?>
 		<label for="Step5">Step 5:</label>
 		<p>
 			</p>
 			<input type="text" id="Step5" name="Step5" placeholder="Enter Step Instructions...">
 		<!-- Dropdown -->
 		<br />
-		<span class="tab"></span><label for="Locationof">Ingredient (optional)</label>
+		<span class="tab"></span><label for="Iname5">Ingredient (optional)</label>
 		<br />
-			<span class="tab"></span><select name="Step5" id="Step5">
+			<span class="tab"></span><select name="Iname5" id="Iname5">
 				<option value = "0" selected = "selected">Select...</option>
 				<?php
 				while($row = mysqli_fetch_row($result)) {	
@@ -190,17 +220,23 @@ echo "please retry with a valid number of steps.";
 		<span class="tab"></span><input type="text" id="ingredamount5" name="ingredamount5" placeholder="5...">
 	<?php } ?>
 	<!-- End Step 5 -->	
-		<!-- Step 6 -->
-	<?php if($Stepnum >= 6) {?>
+	<!-- Step 6 -->
+	<?php if($Stepnum >= 6) {
+		$query = "select Ingredient_name from INGREDIENT";
+		$result = mysqli_query($conn, $query);
+		if (!$result) {
+			die("Query to show fields from table failed");
+		}
+?>
 		<label for="Step6">Step 6:</label>
 		<p>
 			</p>
 			<input type="text" id="Step6" name="Step6" placeholder="Enter Step Instructions...">
 		<!-- Dropdown -->
 		<br />
-		<span class="tab"></span><label for="Locationof">Ingredient (optional)</label>
+		<span class="tab"></span><label for="Iname6">Ingredient (optional)</label>
 		<br />
-			<span class="tab"></span><select name="Step6" id="Step6">
+			<span class="tab"></span><select name="Iname6" id="Iname6">
 				<option value = "0" selected = "selected">Select...</option>
 				<?php
 				while($row = mysqli_fetch_row($result)) {	
@@ -220,17 +256,23 @@ echo "please retry with a valid number of steps.";
 		<span class="tab"></span><input type="text" id="ingredamount6" name="ingredamount6" placeholder="6...">
 	<?php } ?>
 	<!-- End Step 6 -->	
-		<!-- Step 7 -->
-	<?php if($Stepnum >= 7) {?>
+	<!-- Step 7 -->
+	<?php if($Stepnum >= 7) {
+		$query = "select Ingredient_name from INGREDIENT";
+		$result = mysqli_query($conn, $query);
+		if (!$result) {
+			die("Query to show fields from table failed");
+		}
+?>
 		<label for="Step7">Step 7:</label>
 		<p>
 			</p>
 			<input type="text" id="Step7" name="Step7" placeholder="Enter Step Instructions...">
 		<!-- Dropdown -->
 		<br />
-		<span class="tab"></span><label for="Locationof">Ingredient (optional)</label>
+		<span class="tab"></span><label for="Iname7">Ingredient (optional)</label>
 		<br />
-			<span class="tab"></span><select name="Step7" id="Step7">
+			<span class="tab"></span><select name="Iname7" id="Iname7">
 				<option value = "0" selected = "selected">Select...</option>
 				<?php
 				while($row = mysqli_fetch_row($result)) {	
@@ -250,17 +292,23 @@ echo "please retry with a valid number of steps.";
 		<span class="tab"></span><input type="text" id="ingredamount7" name="ingredamount7" placeholder="7...">
 	<?php } ?>
 	<!-- End Step 7 -->	
-		<!-- Step 8 -->
-	<?php if($Stepnum >= 8) {?>
+	<!-- Step 8 -->
+	<?php if($Stepnum >= 8) {
+		$query = "select Ingredient_name from INGREDIENT";
+		$result = mysqli_query($conn, $query);
+		if (!$result) {
+			die("Query to show fields from table failed");
+		}
+?>
 		<label for="Step8">Step 8:</label>
 		<p>
 			</p>
 			<input type="text" id="Step8" name="Step8" placeholder="Enter Step Instructions...">
 		<!-- Dropdown -->
 		<br />
-		<span class="tab"></span><label for="Locationof">Ingredient (optional)</label>
+		<span class="tab"></span><label for="Iname8">Ingredient (optional)</label>
 		<br />
-			<span class="tab"></span><select name="Step8" id="Step8">
+			<span class="tab"></span><select name="Iname8" id="Iname8">
 				<option value = "0" selected = "selected">Select...</option>
 				<?php
 				while($row = mysqli_fetch_row($result)) {	
@@ -280,17 +328,23 @@ echo "please retry with a valid number of steps.";
 		<span class="tab"></span><input type="text" id="ingredamount8" name="ingredamount8" placeholder="8...">
 	<?php } ?>
 	<!-- End Step 8 -->	
-		<!-- Step 9 -->
-	<?php if($Stepnum >= 9) {?>
+	<!-- Step 9 -->
+	<?php if($Stepnum >= 9) {
+		$query = "select Ingredient_name from INGREDIENT";
+		$result = mysqli_query($conn, $query);
+		if (!$result) {
+			die("Query to show fields from table failed");
+		}
+?>
 		<label for="Step9">Step 9:</label>
 		<p>
 			</p>
 			<input type="text" id="Step9" name="Step9" placeholder="Enter Step Instructions...">
 		<!-- Dropdown -->
 		<br />
-		<span class="tab"></span><label for="Locationof">Ingredient (optional)</label>
+		<span class="tab"></span><label for="Iname9">Ingredient (optional)</label>
 		<br />
-			<span class="tab"></span><select name="Step9" id="Step9">
+			<span class="tab"></span><select name="Iname9" id="Iname9">
 				<option value = "0" selected = "selected">Select...</option>
 				<?php
 				while($row = mysqli_fetch_row($result)) {	
@@ -310,6 +364,43 @@ echo "please retry with a valid number of steps.";
 		<span class="tab"></span><input type="text" id="ingredamount9" name="ingredamount9" placeholder="9...">
 	<?php } ?>
 	<!-- End Step 9 -->	
+	<!-- Step 10 -->
+	<?php if($Stepnum >= 10) {
+		$query = "select Ingredient_name from INGREDIENT";
+		$result = mysqli_query($conn, $query);
+		if (!$result) {
+			die("Query to show fields from table failed");
+		}
+?>
+		<label for="Step10">Step 10:</label>
+		<p>
+			</p>
+			<input type="text" id="Step10" name="Step10" placeholder="Enter Step Instructions...">
+		<!-- Dropdown -->
+		<br />
+		<span class="tab"></span><label for="Iname10">Ingredient (optional)</label>
+		<br />
+			<span class="tab"></span><select name="Iname10" id="Iname10">
+				<option value = "0" selected = "selected">Select...</option>
+				<?php
+				while($row = mysqli_fetch_row($result)) {	
+					
+				// $row is array... foreach( .. ) puts every element
+				foreach($row as $cell)
+				echo "<option>";				
+				echo "$cell";	
+				echo "</option>\n";
+				}
+				?>
+			</select>
+			<br />
+		<span class="tab"></span><label for="ingredamount10">Amount of Ingredient (in ounces):</label>
+		<p>
+			</p>
+		<span class="tab"></span><input type="text" id="ingredamount10" name="ingredamount10" placeholder="10...">
+	<?php } ?>
+	<!-- End Step 10 -->	
+	
 	<input type="submit" value="Submit">
 
 </form>
--- PHP/SubmitRecipe.php
+++ PHP/SubmitRecipe.php
@@ -1,1 +1,221 @@
-s
+<? session_start(); ?>
+<!DOCTYPE html>
+<!-- new account -->
+<html>
+<? 
+    include("header.php");
+?>
+
+<style>
+<?php include 'index.css'; ?>
+</style>
+<div class="topnav">
+  <a href="Home.php">Home</a>
+  <a href="RecipesSearch.php">Recipe Search</a>
+  <a class="active" href="AddRecipe.php">Add Recipe</a>
+  <a href="AddIngredient.php">Add Ingredient</a>
+  <a href="LoginPage.php">Login</a>
+  <a href="logout.php">Logout</a>
+  <a href="Account.php">Account</a>
+  <a href="About.php">About</a>
+</div>
+
+<div>
+<h2>Add Ingredient</h2>
+<?php
+	$numstepsagain = $_GET["numstepsagain"];
+// change the value of $dbuser and $dbpass to your username and password
+	include 'connectvarsEECS.php'; 
+	
+	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	if (!$conn) {
+		die('Could not connect: ' . mysql_error());
+	}
+
+	
+	
+	
+// Escape user inputs for security
+	$Rname = mysqli_real_escape_string($conn, $_POST['Rname']);
+	echo $Rname;
+	//oh boy, if loop, we need to use the numstepsagain to get the values we want for here
+	if($numstepsagain >= 1){
+		$Step1 = mysqli_real_escape_string($conn, $_POST['Step1']);
+		$IName1 = mysqli_real_escape_string($conn, $_POST['Iname1']);
+		$ingredamount1 = mysqli_real_escape_string($conn, $_POST['ingredamount1']);
+		echo $Step1;
+		echo $IName1;
+		echo $ingredamount1;
+	}
+	if($numstepsagain >= 2){
+		$Step2 = mysqli_real_escape_string($conn, $_POST['Step2']);
+		$IName2 = mysqli_real_escape_string($conn, $_POST['Iname2']);
+		$ingredamount2 = mysqli_real_escape_string($conn, $_POST['ingredamount2']);
+		echo $Step2;
+		echo $IName2;
+		echo $ingredamount2;
+	}
+	if($numstepsagain >= 3){
+		$Step3 = mysqli_real_escape_string($conn, $_POST['Step3']);
+		$IName3 = mysqli_real_escape_string($conn, $_POST['Iname3']);
+		$ingredamount3 = mysqli_real_escape_string($conn, $_POST['ingredamount3']);
+		echo $Step3;
+		echo $IName3;
+		echo $ingredamount3;
+	}
+	if($numstepsagain >= 4){
+		$Step4 = mysqli_real_escape_string($conn, $_POST['Step4']);
+		$IName4 = mysqli_real_escape_string($conn, $_POST['Iname4']);
+		$ingredamount4 = mysqli_real_escape_string($conn, $_POST['ingredamount4']);
+		echo $Step4;
+		echo $IName4;
+		echo $ingredamount4;
+	}
+	if($numstepsagain >= 5){
+		$Step5 = mysqli_real_escape_string($conn, $_POST['Step5']);
+		$IName5 = mysqli_real_escape_string($conn, $_POST['Iname5']);
+		$ingredamount5 = mysqli_real_escape_string($conn, $_POST['ingredamount5']);
+		echo $Step5;
+		echo $IName5;
+		echo $ingredamount5;
+	}
+	if($numstepsagain >= 6){
+		$Step6 = mysqli_real_escape_string($conn, $_POST['Step6']);
+		$IName6 = mysqli_real_escape_string($conn, $_POST['Iname6']);
+		$ingredamount6 = mysqli_real_escape_string($conn, $_POST['ingredamount6']);
+		echo $Step6;
+		echo $IName6;
+		echo $ingredamount6;
+	}
+	if($numstepsagain >= 7){
+		$Step7 = mysqli_real_escape_string($conn, $_POST['Step7']);
+		$IName7 = mysqli_real_escape_string($conn, $_POST['Iname7']);
+		$ingredamount7 = mysqli_real_escape_string($conn, $_POST['ingredamount7']);
+		echo $Step7;
+		echo $IName7;
+		echo $ingredamount7;
+	}
+	if($numstepsagain >= 8){
+		$Step8 = mysqli_real_escape_string($conn, $_POST['Step8']);
+		$IName8 = mysqli_real_escape_string($conn, $_POST['Iname8']);
+		$ingredamount8 = mysqli_real_escape_string($conn, $_POST['ingredamount8']);
+		echo $Step8;
+		echo $IName8;
+		echo $ingredamount8;
+	}
+	if($numstepsagain >= 9){
+		$Step9 = mysqli_real_escape_string($conn, $_POST['Step9']);
+		$IName9 = mysqli_real_escape_string($conn, $_POST['Iname9']);
+		$ingredamount9 = mysqli_real_escape_string($conn, $_POST['ingredamount9']);
+		echo $Step9;
+		echo $IName9;
+		echo $ingredamount9;
+	}
+	if($numstepsagain >= 10){
+		$Step10 = mysqli_real_escape_string($conn, $_POST['Step10']);
+		$IName10 = mysqli_real_escape_string($conn, $_POST['Iname10']);
+		$ingredamount10 = mysqli_real_escape_string($conn, $_POST['ingredamount10']);
+		echo $Step10;
+		echo $IName10;
+		echo $ingredamount10;
+	}
+	
+	//end age	
+	
+//check for unique Ingredient
+	$numberOfErrors = 0;
+	$queryUnique = "select Ingredient_name from INGREDIENT where Ingredient_name = '$IName'";
+	//check not empty
+	if(empty($Rname) == true){
+		echo "
+		please enter a Name for the recipe\n";
+		$numberOfErrors++;
+	}
+	if($IName1 != 0 && $ingredamount1){
+		echo "Please enter an amount of ingredient 1";
+		$numberOfErrors++;
+	}
+		if($IName2 != 0 && $ingredamount2){
+		echo "Please enter an amount of ingredient 2";
+		$numberOfErrors++;
+	}
+		if($IName3 != 0 && $ingredamount3){
+		echo "Please enter an amount of ingredient 3";
+		$numberOfErrors++;
+	}
+		if($IName4 != 0 && $ingredamount4){
+		echo "Please enter an amount of ingredient 4";
+		$numberOfErrors++;
+	}
+		if($IName5 != 0 && $ingredamount5){
+		echo "Please enter an amount of ingredient 5";
+		$numberOfErrors++;
+	}
+		if($IName6 != 0 && $ingredamount6){
+		echo "Please enter an amount of ingredient 6";
+		$numberOfErrors++;
+	}
+		if($IName7 != 0 && $ingredamount7){
+		echo "Please enter an amount of ingredient 7";
+		$numberOfErrors++;
+	}
+		if($IName8 != 0 && $ingredamount8){
+		echo "Please enter an amount of ingredient 8";
+		$numberOfErrors++;
+	}
+		if($IName9 != 0 && $ingredamount9){
+		echo "Please enter an amount of ingredient 9";
+		$numberOfErrors++;
+	}
+		if($IName10 != 0 && $ingredamount10){
+		echo "Please enter an amount of ingredient 10";
+		$numberOfErrors++;
+	}
+	
+	
+	
+	 //if(empty($cost) == true){
+	//	echo "
+		//please enter a cost per ounce\n";
+		//$numberOfErrors++;
+	//}
+
+	/*
+	//if we have errors, do not let the user proceed
+	if($numberOfErrors < 1) {
+		if(mysqli_query($conn, $queryUnique)){
+			//echo "Connected.";
+			//fbsql_affected_rows
+			$result = mysqli_query($conn, $queryUnique);
+			
+			//testing rows retrieved
+			if (mysqli_num_rows($result) > 0) {
+				echo "
+				Error: ingredient already exists in database";
+				mysqli_close($conn);		
+			} else{
+				// attempt insert query 
+				$query = "INSERT INTO INGREDIENT (Ingredient_name, Ingredient_cost) VALUES ('$IName', $cost)";
+				if(mysqli_query($conn, $query)){
+					echo "New Ingredient added successfully!";
+				} else{
+					echo "ERROR: Could not able to execute $query. " . mysqli_error($conn);
+				}
+			}
+			
+		} else{
+			echo "ERROR: Could not able to execute $queryUnique. " . mysqli_error($conn);
+		}
+	}
+// close connection
+mysqli_close($conn);
+*/
+
+?>
+<div>
+<? 
+    include("footer.php");
+?>
+</html>
+
+	
